2c915f4abd2d512acdc283f2996a547ebb94fd0a
Astrid-Froitier <asfroitier@gmail.com>
2022-11-16 11:23:46 +0100

    created navbar and road to show.html.twig

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/program', name: 'program_')]
+class ProgramController extends AbstractController
+{
+    #[Route('/{id}', requirements:['id' => '\d+'], methods:['GET'], name: 'show')]
+    public function show(): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => 4]);
+    }
+}
